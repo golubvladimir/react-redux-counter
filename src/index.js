ffb55cf91c6a5e324e5bd34b11d969d34d6a47e6
Vladimir Golub <golubvlauri@gmail.com>
2019-09-26 21:58:00 +0200

    add redux to index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from "redux";
+import { Provider } from 'react-redux';
+
+import reducer from './Reducers';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+ReactDOM.render(<Provider store={ store }><App/></Provider>, document.getElementById('root'));
